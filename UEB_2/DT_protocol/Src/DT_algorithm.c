87aa6f6877c7152846c3cdbd5133d7fca6e6df9f
D0kRay <50095938+D0kRay@users.noreply.github.com>
2023-01-12 16:08:35 +0100

    DT algo message send off

--- UEB_2/DT_protocol/Src/DT_algorithm.c
+++ UEB_2/DT_protocol/Src/DT_algorithm.c
@@ -58,12 +58,13 @@ void DT_fillBuffer(list_t *DT_list, uint8_t* buf){
 
 				memcpy(Buffer + i + StatusFlags_OS, &StatusByte, sizeof(uint8_t));
 
-				/*
+
 				if(dt_set->val->Counter == dt_set->val->MaxPackages){
 					//ST_push(dt_set->val->ID);
-					list_remove(DT_list, dt_set);
+//					list_remove(DT_list, dt_set);
+					dt_set->val->F_SendData = F_OFF;
 				}
-				*/
+
 
 				if(dt_set->val->Counter < dt_set->val->MaxPackages)
 					dt_set->val->Counter++;
